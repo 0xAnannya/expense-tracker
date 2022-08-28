6d9b81cf4a6e13e2320d7b1445e108772f118408
0xAnannya <101657035+0xAnannya@users.noreply.github.com>
2022-08-28 17:26:43 +0530

    Header change (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/header";
+import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
 import { TransactionList } from "./components/TransactionList";
